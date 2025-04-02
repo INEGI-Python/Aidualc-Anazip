fcc03a9392878ac0d9a4db3866f70ba6ee690bdb
iscemmanuelitd <isc.emmanuel.itd@gmail.com>
2025-04-02 07:49:47 -0600

    por separado

--- teselas.py
+++ teselas.py
@@ -29,41 +29,66 @@ def generate_hexagon(rectangulo,ancho,alto):
         Hexagonos.extend(clonar_hexagono(hexagono=h,cant=_cant,X=float(-2*ancho),Y=0))
     return Hexagonos
 
-
-
-
 from shapely.geometry import Point,Polygon
 import geopandas as geo
+
+def shp(_a):
+    eval(f"{_a}.to_file('salida/{_a}.shp')")
+
+
 base = geo.read_file("CPV_9_cdmex/CPV_9_cdmex.shp",columns=["ID","POBTOT","geometry"])
 base = base.to_crs(6372)
 base["vtx"]=base.geometry.count_coordinates()
 base = base[base["vtx"] == 5]
-CRS = base.crs.to_string()
-base["ID"]=base.index
 cant= int(base.count().ID)
+base["ID"]=base.index
+base.set_index("ID",inplace=True)
+CRS = base.crs.to_string()
 base["ancho"]=base.geometry.bounds.maxx-base.geometry.bounds.minx
 base["alto"]=base.geometry.bounds.maxy-base.geometry.bounds.miny
-base.set_index("ancho",inplace=True)
-base.sort_index(inplace=True)
+#shp("base")
+#base.set_index("ancho",inplace=True)
+#base.sort_index(inplace=True)
 unirB = base.union_all().centroid
 pts=[unirB,Point(unirB.x+488,unirB.y)]
 centroB = geo.GeoSeries(data=pts,crs=CRS)
-tmp = [base[base.intersects(centroB.iloc[i])].index[0] for i in range(2)]
-ezqui = base.geometry.iloc[tmp[0]].bounds
+shp("centroB")
+b = [int(base[base.intersects(centroB.iloc[0])].index[0])]
+b.append(int(base[base.intersects(centroB.iloc[1])].index[0]))
+print(b)
+centros = base[base["ID"] == b[0]]
+centros.append(base[base["ID"] == b[1]])
+print(centros)
+
+
+
+
+
+
+
+
+
+shp("centros")
+tmp = [int(base[base.intersects(centroB.iloc[i])].index[0]) for i in range(2)]
+
+ezqui = base[base["ID"] == b[0]].geometry.bounds
 ancho = float(ezqui[2] - ezqui[0])
 alto = float(ezqui[3] - ezqui[1])   
 
-union =base.iloc[tmp].union_all()
-
-
+union = centros.union_all()
+print(union)
 unionDF = geo.GeoDataFrame(geometry=[union],crs=CRS)
+shp("unionDF")
+
 centroDF = geo.GeoDataFrame(geometry=[union.centroid],crs=CRS)
+shp("centroDF")
 centro = centroDF["geometry"].iloc[0]
 base["distancia"] = base["geometry"].distance(centro)
 base_ordenada = base.sort_values(by="distancia")
 vecinos = base_ordenada.head(6)
 inicial = vecinos.dissolve(by="vtx")
-
+shp("vecinos")
+shp("inicial")
 total_hexas = generate_hexagon(inicial.geometry.iloc[0],ancho,alto)
 teselas_hexagon = geo.GeoDataFrame(geometry=total_hexas,crs=CRS)
 teselas_hexagon.sindex
